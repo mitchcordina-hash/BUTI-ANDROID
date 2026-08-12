c4435f02b988368e6702ee7e86e430acb304ed45
mitchcordina-hash <mitch.cordina@gmail.com>
2026-08-12 09:12:00 +0200

    Add MainActivity to load local HTML file in WebView

---
+++ app/src/main/java/com/buti/app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.buti.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
